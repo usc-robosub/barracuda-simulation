87d8e3aff08b498bfe8504ae04483af838e8c0cf
Colin Li <ctli@usc.edu>
2024-11-20 12:55:10 -0800

    Add Project DAVE and dependencies

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+# source /opt/ros/noetic/setup.bash
+
+# Build catkin_ws
+# cd barracuda-simulation/catkin_ws
+# catkin build
+# source devel/setup.bash
+
+# Start interactive shell session in /opt/barracuda-simulation directory
+cd /opt/barracuda-simulation
+exec /bin/bash
