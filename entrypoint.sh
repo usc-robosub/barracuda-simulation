3e8cfe32a0a0bd8075aa188ad420e2c21213939e
Matthew Jiang <jiangmy@usc.edu>
2025-01-04 17:01:33 -0800

    Update entrypoint.sh to source Barracuda simulation workspace in bashrc

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
-# source /opt/ros/noetic/setup.bash
+source /opt/ros/noetic/setup.bash
+source /opt/barracuda-simulation/catkin_ws/devel/setup.bash
 
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 echo "source /opt/barracuda-simulation/catkin_ws/devel/setup.bash" >> ~/.bashrc
