678552b7885d0285f3a6a32f1cea71c0a6f2d55a
Colin Li <ctli@usc.edu>
2025-01-13 20:30:01 -0800

    Update barracuda_description submodule

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/bash
-# source /opt/ros/noetic/setup.bash
+source /opt/ros/noetic/setup.bash
+source /opt/barracuda-simulation/catkin_ws/devel/setup.bash
 
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 echo "source /opt/barracuda-simulation/catkin_ws/devel/setup.bash" >> ~/.bashrc
 
 roslaunch barracuda_description barracuda_gazebo.launch
-
-
