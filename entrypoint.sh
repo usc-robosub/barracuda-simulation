91933d4aa755e3ebf7387f1d507e0ac2c7689bd2
mihir <mihirsin@usc.edu>
2025-06-28 22:00:17 +0000

    Removed docker stuff

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-source /opt/ros/noetic/setup.bash
-source /opt/barracuda-simulation/catkin_ws/devel/setup.bash
-
-echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
-echo "source /opt/barracuda-simulation/catkin_ws/devel/setup.bash" >> ~/.bashrc
-
-roslaunch barracuda_description barracuda_gazebo.launch
